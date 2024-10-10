9d796b4924be9bba2fb7fa674a003c76b2630d0a
delilar <delilar45@gmail.com>
2024-10-10 21:44:45 +0300

    атас

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { useCallback } from 'react'
 
 export default function Sidebar() {
+
+    const navigate = useNavigate()
+
+    const handleNavigate = useCallback(() => {
+        navigate(`/`)
+    }, [navigate])
+
     return (
         <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
             <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
-                <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <div onClick={handleNavigate} className="flex items-center gap-3 pl-8 cursor-pointer">
                     <img className='w-6' src={assets.home_icon} alt="Home icon" />
                     <p className='font-bold'>Home</p>
                 </div>
